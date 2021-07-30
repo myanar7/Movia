58746aa588ffa3ef552e11e99540551ca28b8e18
Mustafa Yanar <mustafayanar07.my@gmail.com>
2021-07-30 17:30:49 +0300

    Initial Movie Project

--- MovieApp/MovieApp/SceneDelegate.swift
+++ MovieApp/MovieApp/SceneDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  SceneDelegate.swift
-//  Assignment3
+//  MovieApp
 //
-//  Created by obss on 29.07.2021.
+//  Created by obss on 30.07.2021.
 //
 
 import UIKit
@@ -45,6 +45,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
