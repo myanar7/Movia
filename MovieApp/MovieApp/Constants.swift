4a63da66978cf844c0b40fcbb2d12507b12daf60
Mustafa Yanar <mustafayanar07.my@gmail.com>
2021-07-31 16:14:57 +0300

    Going to intall primarily pods

---
+++ MovieApp/MovieApp/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MovieApp
+//
+//  Created by obss on 30.07.2021.
+//
+
+import Foundation
+
+struct Constants{
+    
+    struct Network {
+        static let apiKey = "71d98b08f7e35fae8bdf6f14d6c2f67d"
+    }
+}
